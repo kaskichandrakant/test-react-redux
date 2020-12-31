baecc6837419bb167b502230b2a8848fbcfad468
Chandrakant Kaski <kaskichandrakant@gmail.com>
2020-12-31 22:13:12 +0530

    add redux enhancer to view store in browser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import todoApp from './reducers'
 import App from './components/App';
 import './index.css'
 
-const store = createStore(todoApp);
+const store = createStore(todoApp, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(
     <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 )
